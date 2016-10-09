7487233df82dbb3b478bd8ee8db86be938513cd5
kzq <kami_ravian@yahoo.com>
2016-10-09 18:50:10 +0100

    Add Ch 13. README.MD

--- Chapter 13 - Organizing a Project/README.md
+++ Chapter 13 - Organizing a Project/README.md
@@ -1,2 +1,72 @@
 # Chapter 13 - Organizing a Project
 ------
+**Mix** |> It is a Elixir project manager similar to ruby bundler. It is a command line utility.
+```elixir
+# new project
+mix new project_name
+# install dependencies
+# cd to project_name
+mix deps.get
+# run tests
+mix test
+# run project
+iex -S mix 
+# recompile and run
+mix run -e "MainModule.main_function" 
+```
+ 
+ **lib/** By convention main module and all application code is stored here.
+ 
+ * create a sub directory with the main module name for example create a directory with name issues in lib/ 
+ * create issues.ex file in lib/ 
+ * All sub modules with in issues will be stored in issues directory
+ * Same is true for other nested modules    
+
+**ex Vs exs** |> Files with extension .ex are compiled files and files with .exs are directly converted into interpreted files. Interpreted files take longer to convert but they run faster.
+
+* We create test files with .exs and source code files with .ex extensions
+
+**ExUnit**|> An Elixir built in testing framework
+```elixir
+defmodule IssuesTest do
+  use ExUnit.Case
+ 
+  test "test me" do
+    assert 1==1 
+  end 
+end
+```
+
+**Hex** |> Hex is a elixir package manager. Elixir packages, applications, libraries are available on http://hex.pm 
+
+**mix.exs** |> It is elixir project's configuration file. It resides in the project folder. **mix.exs** has different functions that return a keyword list. Each keyword specified different settings.
+
+```elixir
+  # mix.exs
+  
+  #project settings
+  def project do
+    [
+      app: :issues,
+      name: "Issues",
+      version: "0.0.1",
+      deps: deps 
+    ]
+  
+  #OTP framework settings
+  def application do
+    [
+      application: [:logger, :htrtpoison]
+    ]
+  end 
+
+  #dependencies  
+  defp deps do
+   {:poison, "~>0.4" }
+  end
+```
+
+  
+
+  
+
--- Chapter 13 - Organizing a Project/issues/config/config.exs
+++ Chapter 13 - Organizing a Project/issues/config/config.exs
@@ -1,32 +1,99 @@
-# This file is responsible for configuring your application
-# and its dependencies with the aid of the Mix.Config module.
-use Mix.Config
+# Chapter 13 - Organizing a Project
+------
+**Mix** |> It is Elixir's project manager, similar to ruby bundler. It is a command line utility.
+```elixir
+# new project
+mix new project_name
+# install dependencies
+# cd to project_name
+mix deps.get
+# list external dependencies
+mix deps
+# run tests
+mix test
+# run project
+iex -S mix 
+# recompile and run
+mix run -e "MainModule.main_function" 
+```
+ 
+ **lib/** By convention main module and all application code is stored here.
+ 
+ * create a sub directory with the main module name for example create a directory with name issues in lib/ 
+ * create issues.ex file in lib/ 
+ * All sub modules with in issues will be stored in issues directory
+ * Same is directory structure true will be applied to other nested modules    
 
-# This configuration is loaded before any dependency and is restricted
-# to this project. If another project depends on this project, this
-# file won't be loaded nor affect the parent project. For this reason,
-# if you want to provide default values for your application for
-# 3rd-party users, it should be done in your "mix.exs" file.
+**ex Vs exs** |> Files with extension .ex are compiled files and files with .exs are directly converted into interpreted files. Interpreted files take longer to convert but they run faster.
 
-# You can configure for your application as:
-#
-#     config :issues, key: :value
-#
-# And access this configuration in your application as:
-#
-#     Application.get_env(:issues, :key)
-#
-# Or configure a 3rd-party app:
-#
-#     config :logger, level: :info
-#
+* We create test files with .exs and source code files with .ex extensions
 
-# It is also possible to import configuration files, relative to this
-# directory. For example, you can emulate configuration per environment
-# by uncommenting the line below and defining dev.exs, test.exs and such.
-# Configuration from the imported file will override the ones defined
-# here (which is why it is important to import them last).
-#
-#     import_config "#{Mix.env}.exs"
+**ExUnit**|> An Elixir built in testing framework
 
-config :issues, github_url: "https://api.github.com"
+```elixir
+defmodule IssuesTest do
+  use ExUnit.Case
+ 
+  test "test me" do
+    assert 1==1 
+  end 
+end
+```
+
+**Hex** |> Hex is Elixir's package manager. Elixir packages, applications, libraries are available ot http://hex.pm 
+
+**mix.exs** |> It is elixir project's configuration file. It resides in the project folder. **mix.exs** has different functions that return a keyword list. Each keyword sets different settings. **mix.lock** file locks the dependencies versions like Gemfile.lock  
+
+```elixir
+  # mix.exs
+  
+  #project settings
+  def project do
+    [
+      app: :issues,
+      name: "Issues",
+      version: "0.0.1",
+      deps: deps 
+    ]
+  
+#OTP framework settings, everything here runs as a     
+#seperate process outside the projects main #process.Managed by OTP
+  def application do
+    [
+      application: [:logger, :httpoison]
+    ]
+  end 
+
+  #dependencies  
+  defp deps do
+   [
+     {:poison, "~>0.4" }
+   ]
+  end
+```
+
+**import_config** |> This function can be used to load dynamic configuration file. Along with **Mix.env**, it can be used to load  different configurations based on the application's environment. 
+
+```elixir
+  use Mix.Config
+  import "#{Mix.env}.exe"
+```
+  
+  **escript** |> It is Erlang's tool that **mix** uses to create a single file of code that can be run on any Unix-based platform. This single file has all dependencies.
+
+* When **escript** runs a program, it looks in your **mix.exs** file for the option escript. This should return a **keyword list** of escript configuration settings. The most important of these is **main_module** which must be set to the name of a module containing a main function.    
+
+**Logger** |>
+
+```elixir
+#config/config.exs
+config :logger, compile_time_purge_level: :info
+
+#in code
+Logger.debug "Debug information"
+Logger.info "Printing document xyz"
+```  
+
+**Testing Comments** |> In Elixir we can test the comments as well. **@doc** is sued to document a function. **## Examples** marks are used to provide tests with in comments.
+
+**Documentation** |> Like ruby's rdoc, Elixir has **ExDoc** - a documentation tool. Simply add **:ex_doc** dependency and run **mix docs** 
